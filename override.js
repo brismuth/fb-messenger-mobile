9b009a908f2493b2a681afc72ae10141fabb3e95
Brian Smith <brismuth@gmail.com>
2020-01-02 11:42:25 -0700

    Create override.js

---
+++ override.js
@@ -0,0 +1,4 @@
+var meta = document.createElement('meta');
+meta.name = "viewport";
+meta.content = "width=device-width, initial-scale=0.6, maximum-scale=0.6";
+document.getElementsByTagName('head')[0].appendChild(meta);
