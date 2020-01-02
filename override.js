c08d6b320168b0c82981c4a13051ea33726226b3
Brian Smith <brismuth@gmail.com>
2020-01-02 11:45:34 -0700

    Delete override.js

--- override.js
+++
@@ -1,4 +0,0 @@
-var meta = document.createElement('meta');
-meta.name = "viewport";
-meta.content = "width=device-width, initial-scale=0.6, maximum-scale=0.6";
-document.getElementsByTagName('head')[0].appendChild(meta);
